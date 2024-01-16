b2f387ce00ce515ca790274820658245222b2103
Jordan <jordane.burton@gmail.com>
2024-01-16 17:12:42 -0600

    finished styling search page

--- assets/js/search/api/settings.js
+++ assets/js/search/api/settings.js
@@ -1,4 +1,5 @@
-const apiKey = "72cb5b31608b4fde9b58b12b834a21a6";
+// const apiKey = "72cb5b31608b4fde9b58b12b834a21a6";
+const apiKey = '30b223bf80be4914bbe992082e3c447a';
 const apiURL = "https://api.spoonacular.com/recipes/"
 const apiKeyString = `apiKey=${apiKey}`;
-const USE_API = false;
+const USE_API = true;
