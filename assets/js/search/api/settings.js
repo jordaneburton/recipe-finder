18adcef6bd33f1630c33d1da21ff4b743e2947a0
Jordan <jordane.burton@gmail.com>
2024-01-16 17:40:45 -0600

    updated all other pages with search page structure/styling

--- assets/js/search/api/settings.js
+++ assets/js/search/api/settings.js
@@ -1,5 +1,4 @@
-// const apiKey = "72cb5b31608b4fde9b58b12b834a21a6";
-const apiKey = '30b223bf80be4914bbe992082e3c447a';
+const apiKey = "72cb5b31608b4fde9b58b12b834a21a6";
 const apiURL = "https://api.spoonacular.com/recipes/"
 const apiKeyString = `apiKey=${apiKey}`;
-const USE_API = true;
+const USE_API = false;
