0a632fbd563d8b22247eefde9e02f5978b3d5866
Jordan <jordane.burton@gmail.com>
2024-01-16 11:51:13 -0600

    fixed up home page and added groceries button to template

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 const apiKey = "72cb5b31608b4fde9b58b12b834a21a6";
 const apiURL = "https://api.spoonacular.com/recipes/"
 const apiKeyString = `apiKey=${apiKey}`;
-const USE_API = true;
+const USE_API = false;
 
 //Grab the ID from URL
 const urlParams = new URLSearchParams(window.location.search);
