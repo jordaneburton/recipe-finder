676c7a91bddf7ba606c2d1cb12254aacd949de65
lukegarnsey <lukeGarnsey@gmail.com>
2024-01-17 10:30:47 -0500

    Update settings.js
    turn on API

--- assets/js/search/api/settings.js
+++ assets/js/search/api/settings.js
@@ -1,4 +1,4 @@
 const apiKey = "72cb5b31608b4fde9b58b12b834a21a6";
 const apiURL = "https://api.spoonacular.com/recipes/"
 const apiKeyString = `apiKey=${apiKey}`;
-const USE_API = false;
+const USE_API = true;
