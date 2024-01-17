9bb27136e201d8e6b21c4bac368c970b42255512
Jordan <jordane.burton@gmail.com>
2024-01-16 19:50:37 -0600

    remove copy button if theres no ingredients

--- assets/js/grocery.js
+++ assets/js/grocery.js
@@ -3,7 +3,7 @@ const savedIngredients = localStorage.getItem("checkedIngredients");
 let checkedIngredients;
 
 // Styles for various elements
-const readyMsgStyles = ['text-xl', 'font-semibold'];
+const readyMsgStyles = ['text-xl', 'font-semibold', 'whitespace-pre-line'];
 const copyButtonStyles = ['text-lg', 'font-semibold', 'rounded-full', 'w-44', 'text-lg', 'bg-lime-500', 'text-white', 'shadow-sm', 'hover:bg-lime-400', 'active:outline', 'active:outline-2', 'active:outline-offset-2', 'active:outline-lime-600'];
 
 //Copy button for ingredient list
@@ -25,21 +25,23 @@ if (checkedIngredients.length > 0) {
     ingredientEl.classList.add("text-center");
     ingredientList.appendChild(ingredientEl);
     
+    const copyButton = document.createElement("button");
+    copyButton.textContent = "Copy Ingredients";
+    copyButton.classList.add("mt-3", 'p-2');
+    ingredientDiv.appendChild(copyButton);
+    for (style of copyButtonStyles) {copyButton.classList.add(style)}
+
+    copyButton.addEventListener("click", function() {
+      const ingredientList = checkedIngredients.join("\n");
+      navigator.clipboard.writeText(ingredientList)
+    });
+
   });
 } else {
   const noIngredients = document.createElement("div");
-  noIngredients.textContent = "Your ready to cook!";
+  noIngredients.textContent = `You have all you need
+
+  You're ready to cook!`;
   ingredientDiv.appendChild(noIngredients);
   for (style of readyMsgStyles) {noIngredients.classList.add(style)}
 }
-
-const copyButton = document.createElement("button");
-copyButton.textContent = "Copy Ingredients";
-copyButton.classList.add("mt-3", 'p-2');
-ingredientDiv.appendChild(copyButton);
-for (style of copyButtonStyles) {copyButton.classList.add(style)}
-
-copyButton.addEventListener("click", function() {
-  const ingredientList = checkedIngredients.join("\n");
-  navigator.clipboard.writeText(ingredientList)
-});
