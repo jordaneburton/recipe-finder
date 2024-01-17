ec05f02c819f1314eb4d0e83a3d703723b80ad56
Jordan <jordane.burton@gmail.com>
2024-01-16 19:42:02 -0600

    styled grocery page

--- assets/js/grocery.js
+++ assets/js/grocery.js
@@ -2,8 +2,11 @@ const savedIngredients = localStorage.getItem("checkedIngredients");
 
 let checkedIngredients;
 
-//Copy button for ingredient list
+// Styles for various elements
+const readyMsgStyles = ['text-xl', 'font-semibold'];
+const copyButtonStyles = ['text-lg', 'font-semibold', 'rounded-full', 'w-44', 'text-lg', 'bg-lime-500', 'text-white', 'shadow-sm', 'hover:bg-lime-400', 'active:outline', 'active:outline-2', 'active:outline-offset-2', 'active:outline-lime-600'];
 
+//Copy button for ingredient list
 
 if (savedIngredients) {
   checkedIngredients = JSON.parse(savedIngredients);
@@ -24,15 +27,17 @@ if (checkedIngredients.length > 0) {
     
   });
 } else {
-  const noIngredients = document.createElement("li");
+  const noIngredients = document.createElement("div");
   noIngredients.textContent = "Your ready to cook!";
-  ingredientList.appendChild(noIngredients);
+  ingredientDiv.appendChild(noIngredients);
+  for (style of readyMsgStyles) {noIngredients.classList.add(style)}
 }
 
 const copyButton = document.createElement("button");
 copyButton.textContent = "Copy Ingredients";
-copyButton.classList.add("mt-3");
+copyButton.classList.add("mt-3", 'p-2');
 ingredientDiv.appendChild(copyButton);
+for (style of copyButtonStyles) {copyButton.classList.add(style)}
 
 copyButton.addEventListener("click", function() {
   const ingredientList = checkedIngredients.join("\n");
